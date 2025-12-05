a72a3205e26cae430fc484d7d4dca71f62807440
Valdir Albino Wallauer <138530577+VAW1970@users.noreply.github.com>
2025-12-05 15:50:35 -0300

    Update settings.py

--- chemstore/settings.py
+++ chemstore/settings.py
@@ -29,6 +29,10 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['.onrender.com'])
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
